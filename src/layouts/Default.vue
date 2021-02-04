d929a01580a7c75d54d442fb5e58ef4d21c1a5b8
freexzw <7924338+freexzw@user.noreply.gitee.com>
2021-02-04 23:24:51 +0800

    initial commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="layout sticky-header">
+    <header class="header sticky" data-v-7e33895e="">
+      <div class="container" data-v-7e33895e="">
+        <div class="left" data-v-7e33895e="">
+          <a
+            href="/"
+            aria-current="page"
+            class="home-link active--exact active"
+            data-v-7e33895e=""
+            ><img
+              src="https://gridsome-forestry.netlify.app/assets/img/logo.332701b7.svg"
+              alt="Nichlas W. Andersen"
+              class="logo"
+              data-v-7e33895e=""
+          /></a>
+        </div>
+        <nav class="nav right" data-v-7e33895e="">
+          <a href="/journal" class="nav__link" data-v-7e33895e="">Journal</a
+          ><a href="/contact" class="nav__link" data-v-7e33895e="">Say Hi!</a>
+        </nav>
+      </div>
+    </header>
+
+    <slot />
+
+    <footer class="footer" data-v-07acb5f8="">
+      <div class="container" data-v-07acb5f8="">
+        <span data-v-07acb5f8="">Copyright © 2021</span>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<style>
+</style>
